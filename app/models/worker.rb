7a9d410fd36ec42a26a8d9a50806146ed19a5deb
Gustavo Gorgônio <ggorgon03@gmail.com>
2024-01-17 05:59:58 -0300

    add workers model validation

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,55 @@
 class Worker < ApplicationRecord
+    validates :name,
+            :presence => true,
+            length: { :minimum => 2 },
+            format: { :with => /\A[a-zA-Z]+\z/, :message => "deve conter apenas letras" }
+    
+    validates :cpf,
+            :presence => true,
+            uniqueness: true,
+            format: {
+                :with => /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/,
+                :message => "deve estar no formato XXX.XXX.XXX-XX"
+            }
+    
+    validates :birthdate, :presence => true
+    
+    validates :salary, :presence => true, numericality: { :greather_than_or_equal_to => 0 }
+    
+    validates :street, :presence => true, length: { :minimum => 5 }
+    
+    validates :number,
+            :presence => true,
+            length: { :minimum => 2 }
+            format: {
+                :with => \A\d+[a-z]*\z,
+                :message => "número inválido" 
+            }
+    
+    validates :district, :presence => true, length: { :minimum => 2 }
+    
+    validates :city, :presence => true, length: { :minimum => 2 }
+    
+    validates :state, :presence => true, length: { :minimum => 2 }
+    
+    validates :zip_code, :presence => true
+            :presence => true,
+            format: {
+                :with => /\A\d{5}\-\d{3}\z/,
+                :message => "deve estar no formato XXXXX-XXX"
+            }
+    
+    validates :personal_phone,
+            :presence => true,
+            format: {
+                :with => /\A(\d{2})\d{5}\-\d{4}\z/,
+                :message => "deve estar no formato (XX)XXXXX-XXXX"
+            }
+    
+    validates :reference_phone,
+            :presence => true,
+            format: {
+                :with => /\A(\d{2})\d{5}\-\d{4}\z/,
+                :message => "deve estar no formato (XX)XXXXX-XXXX"
+            }
 end

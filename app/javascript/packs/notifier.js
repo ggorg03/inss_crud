cb27dfedcc8fb354cf0dff579b6ebfa4014ab276
Gustavo Gorgônio <ggorgon03@gmail.com>
2024-01-21 20:36:23 -0300

    add notifier partial and its js source

---
+++ app/javascript/packs/notifier.js
@@ -0,0 +1,17 @@
+export function notefy(title, content) {
+    let notifier = $("#notifier");
+    let n_title = $("#notifier-title");
+    let n_content = $("#notifier-content");
+    // adding content
+    n_title.html(title);
+    n_content.html(content);
+    // animation
+    notifier.fadeIn(1000, function() {
+        setTimeout(function() {
+            notifier.fadeOut(1000).promise()
+            .then(() => {
+                n_title.html("");
+                n_content.html("");
+            });
+        }, 3000)});
+}

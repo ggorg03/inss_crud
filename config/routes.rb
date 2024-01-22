6db9a7ac13d3089088b404a5893db8ef5e048616
Gustavo Gorgônio <ggorgon03@gmail.com>
2024-01-22 03:49:36 -0300

    add redis and sidekiq to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   post "inss_descount", :to => "workers#inss_descount"
   resources :workers, :path => "workers"
+
   root to: 'workers#index'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
